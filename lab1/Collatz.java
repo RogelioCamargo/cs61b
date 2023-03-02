42d54d46600eaa742562895137fbc9f00207ce7a
RogelioCamargo <rogelio.camargo2016@gmail.com>
2023-03-01 16:50:53 -0800

    Fix nextNumber method in Collatz class

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,14 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Rogelio Camargo
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Method that returns the next number in the Collatz sequence */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n  % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return n * 3 + 1;
         }
     }
 
