0acf596045a8a803682298836b12a1632e2995be
RogelioCamargo <rogelio.camargo2016@gmail.com>
2023-03-11 20:25:29 -0800

    Use implementation inheritance to write a default isEmpty method in Deque class

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -49,10 +49,6 @@ public class ArrayDeque<T> implements Deque<T> {
         size++;
     }
     @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-    @Override
     public int size() {
         return size;
     }
